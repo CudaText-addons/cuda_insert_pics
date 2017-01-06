0ec1184f075d6d27c7e46950fe5bcc0a9b44780b
Alexey-t <support@uvviewsoft.com>
2017-01-06 08:05:48 +0300

    consider gap min-h, max-h

--- __init__.py
+++ __init__.py
@@ -9,7 +9,9 @@ from .imgsize import *
 PIC_TAG = 0x1000 #minimal tag for api (CRC adds to tag)
 BIG_SIZE = 500 #if width bigger, ask to resize
 DIALOG_FILTER = 'Pictures|*.png;*.jpg;*.jpeg;*.jpe;*.gif'
-PREFIX = '[Insert Pics] '
+PRE = '[Insert Pics] '
+MIN_H = 10 #limitations of api to gap height
+MAX_H = 500-5
 API_OK = app_api_version()>='1.0.164'
 
 data_all = {}
@@ -40,22 +42,22 @@ def get_helper_filename(fn):
 
 class Command:
     def insert_dlg(self):
-    
+
         if not API_OK:
             msg_box('Insert Pics plugin needs newer app version', MB_OK+MB_ICONERROR)
             return
-            
+
         fn_ed = ed.get_filename()
         if not fn_ed:
-            msg_status(PREFIX+'Needed named file')
+            msg_status(PRE+'Needed named file')
             return
-    
+
         fn = dlg_file(True, '', '', DIALOG_FILTER)
         if not fn: return
 
         res = get_image_size(fn)
         if not res:
-            msg_status(PREFIX+'Cannot detect picture sizes')
+            msg_status(PRE+'Cannot detect picture sizes')
             return
         size_x, size_y = res
 
@@ -71,18 +73,18 @@ class Command:
         crc = get_file_crc(fn)
         code = get_file_code(fn)
         ntag = PIC_TAG+crc
-        
+
         self.add_dataitem(crc, fn_ed, size_x, size_y, os.path.basename(fn), code)
         self.add_pic(ed, nline, fn, size_x, size_y, ntag)
         ed.set_prop(PROP_MODIFIED, '1')
-        msg_status(PREFIX+'Added "%s", %dx%d, line %d' % (os.path.basename(fn), size_x, size_y, nline))
+        msg_status(PRE+'Added "%s", %dx%d, line %d' % (os.path.basename(fn), size_x, size_y, nline))
 
 
     def add_dataitem(self, crc, fn_ed, size_x, size_y, pic_fn, pic_data):
 
         if fn_ed not in data_all:
-            data_all[fn_ed] = {}    
-        
+            data_all[fn_ed] = {}
+
         data_all[fn_ed][crc] = {
           'size_x': size_x,
           'size_y': size_y,
@@ -93,6 +95,13 @@ class Command:
 
     def add_pic(self, ed, nline, fn, size_x, size_y, ntag):
 
+        new_y = None
+        if size_y < MIN_H: new_y = MIN_H
+        if size_y > MAX_H: new_y = MAX_H
+        if new_y is not None:
+            size_x = round(size_x/size_y*new_y)
+            size_y = new_y
+
         id_bitmap, id_canvas = ed.gap(GAP_MAKE_BITMAP, size_x, size_y)
         canvas_proc(id_canvas, CANVAS_SET_BRUSH, color=0xffffff)
         canvas_proc(id_canvas, CANVAS_RECT_FILL, x=0, y=0, x2=size_x, y2=size_y)
@@ -100,33 +109,33 @@ class Command:
 
         ed.gap(GAP_DELETE, nline, nline)
         ed.gap(GAP_ADD, nline, id_bitmap, tag=ntag)
-        
-        print(PREFIX+'"%s", %dx%d, line %d' % (os.path.basename(fn), size_x, size_y, nline+1))
+
+        print(PRE+'"%s", %dx%d, line %d' % (os.path.basename(fn), size_x, size_y, nline+1))
 
 
     def count_pics(self, ed):
-    
+
         gaps = ed.gap(GAP_GET_LIST, 0, 0)
         if not gaps: return 0
-        
+
         cnt = 0
         for (nline, ntag, size_x, size_y) in gaps:
             crc = ntag-PIC_TAG
             if crc>0:
                 cnt += 1
         return cnt
-    
+
 
     def del_cur(self):
 
         cnt1 = self.count_pics(ed)
         x1, nline, x2, y2 = ed.get_carets()[0]
         ed.gap(GAP_DELETE, nline, nline)
-        
+
         cnt2 = self.count_pics(ed)
         if cnt1!=cnt2:
             ed.set_prop(PROP_MODIFIED, '1')
-            
+
 
     def del_all(self):
 
@@ -138,16 +147,16 @@ class Command:
             if tag>PIC_TAG:
                 ed.gap(GAP_DELETE, y, y)
                 cnt += 1
-            
-        msg_status(PREFIX+'Removed %d pics' % cnt)
+
+        msg_status(PRE+'Removed %d pics' % cnt)
         if cnt>0:
             ed.set_prop(PROP_MODIFIED, '1')
 
 
     def on_open(self, ed_self):
-    
+
         if not API_OK: return
-    
+
         fn_ed = ed_self.get_filename()
         if not fn_ed: return
         fn_helper = get_helper_filename(fn_ed)
@@ -172,11 +181,11 @@ class Command:
             self.add_dataitem(crc, fn_ed, size_x, size_y, pic_fn, pic_data)
             self.add_pic(ed_self, nline, fn_temp, size_x, size_y, ntag)
 
-        msg_status(PREFIX+'Loaded %d pics' % len(data_this))
+        msg_status(PRE+'Loaded %d pics' % len(data_this))
 
 
     def on_save(self, ed_self):
-    
+
         fn_ed = ed_self.get_filename()
         if not fn_ed: return
         fn_helper = get_helper_filename(fn_ed)
@@ -185,7 +194,7 @@ class Command:
 
         gaps = ed_self.gap(GAP_GET_LIST, 0, 0)
         if not gaps: return
-        
+
         data_ed = data_all.get(fn_ed, None)
         if not data_ed: return
 
